a3bf035818e6193a04e736ba3e421da1227cade3
Sidhanth Pandey <sidhanthpandey@192.168.1.3>
2024-10-05 23:45:46 +0530

    Add shadcn component

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,6 @@
-import localFont from "next/font/local";
-import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { ThemeProvider } from "./components/theme-provider";
+import "./globals.css";
 
 export const metadata = {
   title: "Create Next App",
@@ -21,9 +11,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
